7b23bf710870e4da1144f1d6db52d82d1c059a12
gktnkrdg <gktnkrdg@gmail.com>
2018-10-27 20:27:37 +0300

    api hook

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -8,5 +8,12 @@ var todos = require('./api/todos.route')
 router.use('/movies', movies);
 router.use('/restaurants', restaurants);
 router.use('/todos', todos);
-
+router.use('/github',  function (req, res) {
+    var sender = req.body.sender;
+    var branch = req.body.ref;
+  
+    if(branch.indexOf('master') > -1){
+        console.log("works")
+    }
+  })
 module.exports = router;
