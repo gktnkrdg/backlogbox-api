cd4f2845ad3ab4f43905c91e2ede51116001fc4d
gktnkrdg <gktnkrdg@gmail.com>
2018-10-28 04:37:43 +0300

    comment

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use('/users', users);
 app.use('/api', api);
 
 app.use(function(err, req, res, next) {
-  
+  //
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
