5acc4bc11206324141b397e01f972d27bf130d6e
gktnkrdg <gktnkrdg@gmail.com>
2018-10-28 03:48:18 +0300

    shell chmod

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -18,7 +18,7 @@ router.use('/github',  function (req, res) {
     }
   })
   function deploy(res){
-    chmod('u+x', '~/backlogbox/backlogbox-api/deploy.sh');
+    shell.chmod('u+x', '~/backlogbox/backlogbox-api/deploy.sh');
     shell.exec('~/backlogbox/backlogbox-api/deploy.sh', function(err, stdout, stderr){
         if (err) {
          console.error(err);
