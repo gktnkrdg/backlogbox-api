c1d81fb1d5d411993bb2ac7fdd990c15ea8809ee
gokten <gktnkrdg@gmail.com>
2018-10-30 17:07:45 +0300

    ROUTER ON

--- src/routes/api/movies.route.js
+++ src/routes/api/movies.route.js
@@ -6,7 +6,7 @@ var MovieController = require('../../controllers/movie.controller');
 
 router.get('/', MovieController.getMovies)
 router.post('/', MovieController.createMovie)
-// router.put('/', MovieController.updateMovie)
-// router.delete('/:id',MovieController.removeMovie)
+router.put('/', MovieController.updateMovie)
+router.delete('/:id',MovieController.removeMovie)
 
 module.exports = router;
