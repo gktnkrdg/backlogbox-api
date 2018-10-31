379b1f6aa959b87baddcbd38cd6d1c290460bc63
gokten <gktnkrdg@gmail.com>
2018-10-31 14:45:06 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -22,10 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", " Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin,Accept,Content-Type, Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 app.use(compression());
